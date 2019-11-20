8e023892887c0f6e4d664f537b69abcfe5fd615d
satoshi-r <rgeargerge@yandex.ru>
2019-11-21 01:57:07 +0300

    added tabs

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-	// Custom JS
+	let tab = document.getElementsByClassName('scientific__tabs-item'),
+		tabContent = document.getElementsByClassName('scientific__tab-content'),
+		info = document.getElementsByClassName('scientific__tabs')[0];
+
+	function hideTabContent(a) {
+		for (let i = a; i < tabContent.length; i++) {
+			tabContent[i].classList.remove('show');
+			tabContent[i].classList.add('hide');
+		}
+	}
+
+	hideTabContent(1);
+
+	function showTabContent(b) {
+		if (tabContent[b].classList.contains('hide')) {
+			hideTabContent(0);
+			tabContent[b].classList.remove('hide');
+			tabContent[b].classList.add('show');
+		}
+	}
+
+	info.addEventListener('click', function (event) {
+		let target = event.target;
+		if (target.className == 'scientific__tabs-item') {
+			for (let i = 0; i < tab.length; i++) {
+				if (target == tab[i]) {
+					showTabContent(i);
+					break;
+				}
+			}
+		}
+	});
 
 });
