20ca372b23340257290935df3db5bda1ce07435d
satoshi-r <rgeargerge@yandex.ru>
2019-12-06 03:05:22 +0300

    add scrollTo

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -120,4 +120,21 @@ document.addEventListener("DOMContentLoaded", function () {
 	});
 
 
+	// ScrollTo
+	const anchors = document.querySelectorAll('a[href*="#"]')
+
+	for (let anchor of anchors) {
+		anchor.addEventListener('click', function (e) {
+			e.preventDefault()
+
+			const blockID = anchor.getAttribute('href').substr(1)
+
+			document.getElementById(blockID).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			})
+		})
+	}
+
+
 });
